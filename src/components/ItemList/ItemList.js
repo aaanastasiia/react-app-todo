6b34e0997ffc6dcee971c77913f730dce0860fb5
Anishchik Anastasiia <an.anischik@gmail.com>
2021-06-18 09:14:59 +0300

    bug fix in ToDo

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,67 +1,88 @@
-import React from 'react';
-import Item from '../Item/Item';
-import styles from './ItemList.module.css';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
-import CancelIcon from '@material-ui/icons/Cancel';
-import frame from './img/frame.png';
-import PropTypes from 'prop-types';
+import React from "react";
+import Item from "../Item/Item";
+import styles from "./ItemList.module.css";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import CancelIcon from "@material-ui/icons/Cancel";
+import frame from "./img/frame.png";
+import PropTypes from "prop-types";
 
 class ItemList extends React.Component {
   render() {
     const { onClickDone, onClickDelete, filter } = this.props;
 
-      return (
-        <div>
-          {filter.length !== 0
-            ? <ul className={styles.wrap}>
-              {filter.map(item => (<li key={item.value}>
+    return (
+      <div>
+        {filter?.length !== 0 ? (
+          <ul className={styles.wrap}>
+            {filter.map((item) => (
+              <li key={item.value}>
                 <div className={styles.item__wrap}>
-                  <FormControlLabel className={styles.checkbox}
+                  <FormControlLabel
+                    className={styles.checkbox}
                     control={
-                        <Checkbox
-                            checked={item.isDone}
-                            icon={<RadioButtonUncheckedIcon className={styles.checkbox__icon}/>}
-                            checkedIcon={<CheckCircleIcon className={styles.checkbox__icon}/>}
-                            value="checkedB"
-                            color="primary"
-                            onClick={() => onClickDone(item.id)}
-                        />
+                      <Checkbox
+                        checked={item.isDone}
+                        icon={
+                          <RadioButtonUncheckedIcon
+                            className={styles.checkbox__icon}
+                          />
+                        }
+                        checkedIcon={
+                          <CheckCircleIcon className={styles.checkbox__icon} />
+                        }
+                        value="checkedB"
+                        color="primary"
+                        onClick={() => onClickDone(item.id)}
+                      />
                     }
-                      label={<Item 
-                              value={item.value}
-                              isDone={item.isDone}
-                              id={item.id}
-                              onClickDone={onClickDone}
-                              onClickDelete={onClickDelete}
-                            />}
-                    />
-                    <Tooltip title="Удалить" onClick={() => onClickDelete(item.id)}>
-                      <IconButton aria-label="delete">
-                        <CancelIcon fontSize="small" className={styles.delete__icon}/>
-                      </IconButton>
-                    </Tooltip>
+                    label={
+                      <Item
+                        value={item.value}
+                        isDone={item.isDone}
+                        id={item.id}
+                        onClickDone={onClickDone}
+                        onClickDelete={onClickDelete}
+                      />
+                    }
+                  />
+                  <Tooltip
+                    title="Удалить"
+                    onClick={() => onClickDelete(item.id)}
+                  >
+                    <IconButton aria-label="delete">
+                      <CancelIcon
+                        fontSize="small"
+                        className={styles.delete__icon}
+                      />
+                    </IconButton>
+                  </Tooltip>
                 </div>
               </li>
-              ))}
-            </ul>
-            : <div className={styles.noItems}>
-                <img className={styles.noItems__img} alt='/' src={frame}></img>
-                <p className={styles.noItems__text}>Вы ещё не добавили ни одной задачи</p>
-                <p className={styles.noItems__now}>Сделайте это прямо сейчас!</p>
-            </div>}
-        </div>);
+            ))}
+          </ul>
+        ) : (
+          <div className={styles.noItems}>
+            <img className={styles.noItems__img} alt="/" src={frame}></img>
+            <p className={styles.noItems__text}>
+              Вы ещё не добавили ни одной задачи
+            </p>
+            <p className={styles.noItems__now}>Сделайте это прямо сейчас!</p>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
 ItemList.propTypes = {
   items: PropTypes.array.isRequired,
   onClickDone: PropTypes.func.isRequired,
-  onClickDelete: PropTypes.func.isRequired
+  onClickDelete: PropTypes.func.isRequired,
 };
 
 export default ItemList;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import Footer from '../Footer/Footer';
-import styles from './Todo.module.css';
-import Card from '@material-ui/core/Card';
+import React, { useState, useEffect } from "react";
+import InputItem from "../InputItem/InputItem";
+import ItemList from "../ItemList/ItemList";
+import Footer from "../Footer/Footer";
+import styles from "./Todo.module.css";
+import Card from "@material-ui/core/Card";
 
 const Todo = () => {
   const initialState = {
     items: [],
     count: 0,
-    filteredItems: 'Все'
+    filteredItems: "Все",
   };
 
   const [items, setItems] = useState(initialState.items);
@@ -17,89 +17,91 @@ const Todo = () => {
   const [filteredItems, setFilter] = useState(initialState.filteredItems);
 
   useEffect(() => {
-    const items = localStorage.getItem('items');
+    const items = localStorage.getItem("items");
     setItems(JSON.parse(items));
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('items', JSON.stringify(items));
+    localStorage.setItem("items", JSON.stringify(items));
   }, [items]);
 
-  const onClickDone = id => {
-    const newItemList = items.map(item => {
+  const onClickDone = (id) => {
+    const newItemList = items.map((item) => {
       const newItem = { ...item };
 
-        if (item.id === id) {
-          newItem.isDone = !item.isDone;
-        }
+      if (item.id === id) {
+        newItem.isDone = !item.isDone;
+      }
 
-        return newItem;
+      return newItem;
     });
 
     setItems(newItemList);
   };
-  
-  const onClickDelete = id => {
-    const newItemList = items.filter(item => item.id !== id);
+
+  const onClickDelete = (id) => {
+    const newItemList = items.filter((item) => item.id !== id);
     setItems(newItemList);
-    setCount(count => count - 1);
+    setCount((count) => count - 1);
   };
 
-  const onClickAdd = value => {
+  const onClickAdd = (value) => {
     const newItemList = [
       ...items,
       {
         value,
         isDone: false,
-        id: count + 1
-      }
+        id: count + 1,
+      },
     ];
     setItems(newItemList);
     setCount(count + 1);
   };
 
-  const all = items;
-  const activeItems = items.filter(item => !item.isDone);
-  const doneItems = items.filter(item => item.isDone);
+  const all = items?.length;
+  const activeItems = items?.filter((item) => !item.isDone);
+  const doneItems = items?.filter((item) => item.isDone);
 
-  const onClickFilter = filtration => setFilter(filtration);
+  const onClickFilter = (filtration) => setFilter(filtration);
 
   let filter;
-    switch (filteredItems) {
-        case 'Завершенные':
-            filter = items.filter(item => item.isDone);
-            break;
-        case 'Незавершенные':
-            filter = items.filter(item => !item.isDone);
-            break;
-        default: 
-            filter = items;
-            break;
-   }
- 
-    return (
-      <Card className={styles.card}>
-        <div className={styles.wrap}>
-            <div className={styles.content}>
-                <Footer 
-                    countAll={all.length} 
-                    onClickFilter={onClickFilter} 
-                    countActiveItems={activeItems.length} 
-                    countDoneItems={doneItems.length}
-                    filteredItems={filteredItems} />
-                <div className={styles.items}>
-                    <ItemList 
-                        items={items} 
-                        onClickDone={onClickDone} 
-                        onClickDelete = {onClickDelete}
-                        filter={filter}
-                        filteredItems={filteredItems} />
-                    <InputItem onClickAdd={onClickAdd} items={items} />
-                </div>
-            </div>
-        </div>
-      </Card>);
-};
+  switch (filteredItems) {
+    case "Завершенные":
+      filter = items?.filter((item) => item.isDone);
+      break;
+    case "Незавершенные":
+      filter = items?.filter((item) => !item.isDone);
+      break;
+    default:
+      filter = items;
+      break;
+  }
 
+  return (
+    <Card className={styles.card}>
+      <div className={styles.wrap}>
+        <div className={styles.content}>
+          <Footer
+            countAll={all}
+            onClickFilter={onClickFilter}
+            countActiveItems={activeItems?.length}
+            countDoneItems={doneItems?.length}
+            filteredItems={filteredItems}
+          />
+          <div className={styles.items}>
+            <ItemList
+              items={items}
+              onClickDone={onClickDone}
+              onClickDelete={onClickDelete}
+              filter={filter}
+              filteredItems={filteredItems}
+            />
+            <InputItem onClickAdd={onClickAdd} items={items} />
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
 
 export default Todo;
